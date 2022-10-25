f353d3abdda4752506cb5306d0be0681636a1b92
Robert Stoelhorst <robertstoelhorst@gmail.com>
2022-10-25 11:04:06 +1100

    edit readme and hyperlinks

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PHP-Playground
 
 #### Live @ [test-php-playground.herokuapp.com](https://test-php-playground.herokuapp.com/)
+
+**_ Merge to the main branch to deploy _**
+I have integrated this github repo with my Heroku account.
+In the heroku project webapp configuration I have set up automatic deployments from the main branch, so any merges to main will start a production build and deploy process.
+All development work should be done in the Dev branch and tested locally for now.
--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-hello world!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Play</title>
+</head>
+
+<body>
+    <div>
+        <h1>Hello Playground</h1>
+    </div>
+    <?php
+    session_start();
+    function kill_session()
+    {
+        unset($_SESSION['user']);
+        unset($_SESSION['pass']);
+        session_destroy();
+        echo '<hr>Session Destroyed<br>';
+        echo  'Session ID : ' . session_id() . '<br>';
+        var_dump($_SESSION);
+    }
+    if (count($_SESSION) > 0) {
+        echo '<dl> ';
+        foreach ($_SESSION as $key => $value) {
+            echo "<dt>Key: $key";
+            echo "<dd>Value: $value";
+        }
+        echo '</dl><hr>';
+        echo 'Session ID : ' . session_id() . '<br>';
+        var_dump($_SESSION);
+        kill_session();
+    } else {
+        echo 'Please <a href="./mysql/login.php">Log In</a>';
+    }
+    ?>
+</body>
+
+</html>
